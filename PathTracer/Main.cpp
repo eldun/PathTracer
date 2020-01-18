5dc6858f2d9c0a8eb3602ec8793646165cbdab20
dunneev <dunneev@gmail.com>
2020-01-17 23:34:04 -0500

    Comments for .ppm formatting

--- PathTracer/Main.cpp
+++ PathTracer/Main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main() {
-	int nx = 200;
-	int ny = 100;
-	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-	for (int j = ny - 1; j >= 0; j--) {
+	int nx = 200; // Number of horizontal pixels
+	int ny = 100; // Number of vertical pixels
+	std::cout << "P3\n" << nx << " " << ny << "\n255\n"; // P3 signifies ASCII, 255 signifies max color value
+	for (int j = ny - 1; j >= 0; j--) { // RGB triplets
 		for (int i = 0; i < nx; i++) {
 			float r = float(i) / float(nx);
 			float g = float(j) / float(ny);
