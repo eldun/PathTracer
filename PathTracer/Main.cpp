9e19cebed4504162f89ac6ca2709a77931ee666a
dunneev <dunneev@gmail.com>
2020-01-22 11:11:02 -0500

    Update main to use vec3

--- PathTracer/Main.cpp
+++ PathTracer/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Vec3.h"
 
 int main() {
 	int nx = 200; // Number of horizontal pixels
@@ -6,12 +7,10 @@ int main() {
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n"; // P3 signifies ASCII, 255 signifies max color value
 	for (int j = ny - 1; j >= 0; j--) { // RGB triplets
 		for (int i = 0; i < nx; i++) {
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2;
-			int ir = int(255.99 * r);
-			int ig = int(255.99 * g);
-			int ib = int(255.99 * b);
+			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
 			std::cout << ir << " " << ig << " " << ib << "\n";
 		}
 	}
