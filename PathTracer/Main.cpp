530cdcbbc17632a2fb33c901166c8d210caa9850
dunneev <dunneev@gmail.com>
2020-01-25 15:24:04 -0500

    create background image using lerp, add illustration, fix typos in vec3.h

--- PathTracer/Main.cpp
+++ PathTracer/Main.cpp
@@ -1,13 +1,44 @@
 #include <iostream>
-#include "Vec3.h"
+#include "ray.h"
+
+/****************************************************************************************
+The code for this path tracer is based on "Ray Tracing in One Weekend" by Peter Shirley. 
+				https://github.com/RayTracing/raytracing.github.io
+*****************************************************************************************/
+
+/*
+* Linearly blends white and blue depending on the value of y coordinate (Linear Blend/Linear Interpolation/lerp).
+* Lerps are always of the form: blended_value = (1-t)*start_value + t*end_value.
+* t = 0.0 = White
+* t = 1.0 = Blue
+*/
+vec3 color(const ray& r) {
+	vec3 unit_direction = unit_vector(r.direction());
+	double t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+
+}
 
 int main() {
 	int nx = 200; // Number of horizontal pixels
 	int ny = 100; // Number of vertical pixels
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n"; // P3 signifies ASCII, 255 signifies max color value
+
+	// The values below are derived from making the "camera"/ray origin coordinates (0, 0, 0) relative to the canvas. 
+	// See the included file "TracingIllustration.png" for a visual representation.
+	vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	vec3 horizontal(4.0, 0.0, 0.0);
+	vec3 vertical(0.0, 2.0, 0.0);
+	vec3 origin(0.0, 0.0, 0.0);
 	for (int j = ny - 1; j >= 0; j--) { // RGB triplets
 		for (int i = 0; i < nx; i++) {
-			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+			double u = double(i) / double(nx);
+			double v = double(j) / double(ny);
+
+			// Approximate pixel centers on the canvas for each ray r
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+
+			vec3 col = color(r);
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
 			int ib = int(255.99 * col[2]);
--- PathTracer/Vec3.h
+++ PathTracer/Vec3.h
@@ -10,8 +10,6 @@ public:
 	vec3() {}
 	vec3(double e0, double e1, double e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
 
-	// inline to define globals within header
-	// const to ensure values can't be changed
 	inline double x() const { return e[0]; }
 	inline double y() const { return e[1]; }
 	inline double z() const { return e[2]; }
@@ -37,10 +35,10 @@ public:
 	inline vec3& operator/=(const double t);
 
 	inline double length() const {
-		return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+		return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
 	}
 	inline double squared_length() const {
-		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+		return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
 	}
 	inline void make_unit_vector();
 
@@ -60,26 +58,26 @@ inline std::ostream& operator<<(std::ostream& os, const vec3& t) {
 
 
 inline void vec3::make_unit_vector() {
-	double k = 1.0 / sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+	double k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
 	e[0] *= k;
 	e[1] *= k;
 	e[2] *= k;
 }
 
 inline vec3 operator+(const vec3& v1, const vec3& v2) {
-	return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[2], v1.e[3] + v2.e[3]);
+	return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
 }
 
 inline vec3 operator-(const vec3& v1, const vec3& v2) {
-	return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[2], v1.e[3] - v2.e[3]);
+	return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
 }
 
 inline vec3 operator*(const vec3& v1, const vec3& v2) {
-	return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[2], v1.e[3] * v2.e[3]);
+	return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
 }
 
 inline vec3 operator/(const vec3& v1, const vec3& v2) {
-	return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[2], v1.e[3] / v2.e[3]);
+	return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
 }
 
 inline vec3 operator*(double t, const vec3& v) {
@@ -96,12 +94,15 @@ inline vec3 operator*(const vec3& v, double t) {
 
 // Dot product
 inline double dot(const vec3& v1, const vec3& v2) {
-	return v1.e[0] * v2.e[0] + v1.e[0] * v2.e[0] + v1.e[0] * v2.e[0];
+	return 
+		v1.e[0] * v2.e[0] 
+		+ v1.e[1] * v2.e[1] 
+		+ v1.e[2] * v2.e[2];
 }
 
 inline vec3 cross(const vec3& v1, const vec3& v2) {
 	return vec3((v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1]),
-		(-(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0])),
+		(-(v1.e[2] * v2.e[0] - v1.e[0] * v2.e[2])),
 		(v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]));
 }
 
