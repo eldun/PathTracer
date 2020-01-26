e9ed7c54f8ed0868fa4159e1a65d0f9e11cdde70
dunneev <dunneev@gmail.com>
2020-01-26 11:55:41 -0500

    Draw red sphere

--- PathTracer/Main.cpp
+++ PathTracer/Main.cpp
@@ -6,6 +6,33 @@ The code for this path tracer is based on "Ray Tracing in One Weekend" by Peter 
 				https://github.com/RayTracing/raytracing.github.io
 *****************************************************************************************/
 
+/* 
+* Color any pixel red that hits a small sphere at -1 on the z-axis. 
+*
+* Equation of sphere in vector form : dot((p-c, (p-c)) = R^2 
+* where p is a point on the ray, c is the center of the sphere, and R is the radius of the sphere.
+* This can be read as "any point p that satisfies this equation is on the sphere."
+* We want to know if our ray ever hits the sphere, so we are looking for any t where the following is true:
+* dot((p(t)-c, (p(t)-c)) = R^2 
+*
+* Expanding the full form of the ray p(t), we get:
+* dot((a + t*b -c), (a + t*b -c)) = R^2
+* or:
+* t*t*dot( B , B ) + 2*t*dot( B,A - C ) + dot( A-C,A - C ) - R*R = 0
+*
+* solving the quadratic equation for the unknown (t), will result 
+* in a square root that is positive(two solutions), negative(no solutions), or zero(1 solution). See Quadratic.png for a visual.
+* I haven't done geometry in a while.
+*/
+bool hit_sphere(const vec3& center, double radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+	double a = dot(r.direction(), r.direction());
+	double b = 2.0 * dot(oc, r.direction());
+	double c = dot(oc, oc) - radius * radius;
+	double discriminant = b*b - 4*a*c;
+	return (discriminant > 0);
+}
+
 /*
 * Linearly blends white and blue depending on the value of y coordinate (Linear Blend/Linear Interpolation/lerp).
 * Lerps are always of the form: blended_value = (1-t)*start_value + t*end_value.
@@ -13,6 +40,8 @@ The code for this path tracer is based on "Ray Tracing in One Weekend" by Peter 
 * t = 1.0 = Blue
 */
 vec3 color(const ray& r) {
+	if (hit_sphere(vec3(0, 0, -1), 0.5, r))
+		return vec3(1, 0, 0);
 	vec3 unit_direction = unit_vector(r.direction());
 	double t = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
