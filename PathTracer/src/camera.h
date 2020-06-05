f877432be598fe0b5dfad8d82c2310c694e43d77
dunneev <dunneev@gmail.com>
2020-06-05 17:16:43 -0400

    Change implementation of RNG for clarity

--- PathTracer/src/camera.h
+++ PathTracer/src/camera.h
@@ -11,7 +11,7 @@
 vec3 random_unit_sphere_coordinate() {
 	vec3 p;
 	do {
-		p = 2.0 * vec3(random_double(0.0, 0.999), random_double(0.0, 0.999), random_double(0.0, 0.999)) - vec3(1, 1, 1);
+		p = 2.0 * vec3(random_double(0, 1), random_double(0, 1), random_double(0, 1)) - vec3(1, 1, 1);
 	} while (p.squared_length() >= 1.0);
 	return p;
 }
--- PathTracer/src/random.h
+++ PathTracer/src/random.h
@@ -2,16 +2,15 @@
 #define RANDOMH
 
 #include <cstdlib>
-#include <random>
 
-double random_double(double min, double max) {
-    max *= 1000.0;
-    std::random_device dev;
-    std::mt19937 rng(dev()); //Mersenne Twister pseudo-random generator of 32-bit numbers with a state size of 19937 bits
-    std::uniform_int_distribution<std::mt19937::result_type> dist6(min, max); // distribution in range [min, max]
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
 
-    return dist6(rng) / 1000.0;
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
 }
 
 #endif // !RANDOMH
-
