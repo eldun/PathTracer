ce42bd040c818266917c472d97acf29d9c77af56
Evan <doonee63@gmail.com>
2023-06-11 15:15:44 -0400

    Create Texture class

---
+++ src/Texture.h
@@ -0,0 +1,27 @@
+#ifndef TEXTUREH
+#define TEXTUREH
+
+#include "RtWeekend.h"
+
+class Texture {
+    public:
+        virtual Vec3 value(double u, double v, const Vec3& p) const = 0;
+};
+
+class SolidColor : public Texture {
+    public:
+        SolidColor() {}
+        SolidColor(Vec3 c) : colorValue(c) {}
+
+        SolidColor(double red, double green, double blue)
+          : SolidColor(Vec3(red,green,blue)) {}
+
+        virtual Vec3 value(double u, double v, const Vec3& p) const override {
+            return colorValue;
+        }
+
+    private:
+        Vec3 colorValue;
+};
+
+#endif
