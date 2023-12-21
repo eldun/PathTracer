4cf28107569dd2b82b03e61fe5323c0ab2f4aba1
dunneev <dunneev@gmail.com>
2023-12-20 21:15:16 -0500

    Add more scene renders

--- src/Main.cpp
+++ src/Main.cpp
@@ -98,15 +98,93 @@ void generateRandomScene(bool useBvh = true) {
 
 }
 
+void generateRandomBouncyScene() {
+    HittableList world;
+
+        auto checker = make_shared<CheckerTexture>(0.32, Vec3(.2, .3, .1), Vec3(.9, .9, .9));
+        world.add(make_shared<Sphere>(Vec3(0,-1000,0), 1000, make_shared<Lambertian>(checker)));
+
+        for (int a = -11; a < 11; a++) {
+            for (int b = -11; b < 11; b++) {
+                auto choose_mat = randomDouble();
+                Vec3 center(a + 0.9*randomDouble(), 0.2, b + 0.9*randomDouble());
+
+                if ((center - Vec3(4, 0.2, 0)).length() > 0.9) {
+                    shared_ptr<Material> sphereMaterial;
+
+                    if (choose_mat < 0.8) {
+                        // diffuse
+                        auto albedo = Vec3::random() * Vec3::random();
+                        sphereMaterial = make_shared<Lambertian>(albedo);
+                        auto center2 = center + Vec3(0, randomDouble(0,.5), 0);
+                        world.add(make_shared<Sphere>(center, center2, 0, 1, 0.2, sphereMaterial));
+                    } else if (choose_mat < 0.95) {
+                        // metal
+                        auto albedo = Vec3::random(0.5, 1);
+                        auto fuzz = randomDouble(0, 0.5);
+                        sphereMaterial = make_shared<Metal>(albedo, fuzz);
+                        world.add(make_shared<Sphere>(center, 0.2, sphereMaterial));
+                    } else {
+                        // glass
+                        sphereMaterial = make_shared<Dielectric>(1.5);
+                        world.add(make_shared<Sphere>(center, 0.2, sphereMaterial));
+                    }
+                }
+            }
+        }
+
+        auto material1 = make_shared<Dielectric>(1.5);
+        world.add(make_shared<Sphere>(Vec3(0, 1, 0), 1.0, material1));
+
+        auto material2 = make_shared<Lambertian>(Vec3(0.4, 0.2, 0.1));
+        world.add(make_shared<Sphere>(Vec3(-4, 1, 0), 1.0, material2));
+
+        auto material3 = make_shared<Metal>(Vec3(0.7, 0.6, 0.5), 0.0);
+        world.add(make_shared<Sphere>(Vec3(4, 1, 0), 1.0, material3));
+
+        world = HittableList(make_shared<BvhNode>(world, 0.0, 1.0));
+
+        Camera cam;
+        cam.render(world);
+
+
+    }
+
+void generateTwoSpheres() {
+    HittableList world;
+
+    auto checker = make_shared<CheckerTexture>(0.8, Vec3(.2, .3, .1), Vec3(.9, .9, .9));
+
+    world.add(make_shared<Sphere>(Vec3(0,-10, 0), 10, make_shared<Lambertian>(checker)));
+    world.add(make_shared<Sphere>(Vec3(0, 10, 0), 10, make_shared<Lambertian>(checker)));
+
+    Camera cam;
+
+    cam.vFov        = 20;
+    cam.lookFrom    = Vec3(13,2,3);
+    cam.lookAt      = Vec3(0,0,0);
+    cam.upDirection = Vec3(0,1,0);
+
+    cam.render(world);
+}
+
 int main() {
 
-    switch (0) {
+    switch (3) {
     case 0:
         generateRandomScene();
         break;
 
     case 1:
+        generateRandomBouncyScene();
+        break;
+
+    case 2:
         generateMovingSphereComparisonScene();
+        break;
+
+    case 3:
+        generateTwoSpheres();
         break;
 
     default:
